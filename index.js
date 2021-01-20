18c4a94be4b4bbd80f80a7104e4f68c497bda77c
kimjaehyen <mine6546@gmail.com>
2021-01-21 01:15:12 +0900

    textquery를 라우트하는 코드 추가하고 dialogflow에 필요한 config 설정

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
 const app = express();
 
-const config = require('./server/config/keys');
+const config = require("./server/config/keys");
 
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.use('/api/dialogflow', require('./server/routes/dialogflow'));
+app.use("/api/dialogflow", require("./server/routes/dialogflow"));
 
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server Running at ${port}`);
+});
---
+++ server/routes/dialogflow.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router();
+// const structjson = require("./structjson.js");
+const dialogflow = require("dialogflow");
+// const uuid = require("uuid");
+
+const config = require("../config/keys");
+
+const projectId = config.googleProjectID;
+const sessionId = config.dialogFlowSessionID;
+const languageCode = config.dialogFlowSessionLanguageCode;
+
+// Create a new session
+const sessionClient = new dialogflow.SessionsClient();
+const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+// 텍스트 쿼리 라우트
+router.post("/textquery", async (req, res) => {
+  // 클라이언트에서 받은 정보를 dialogflow에 보냄
+  // dialogflow에 예제를 가져온다
+
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        // POSTMAN으로 테스트시 프로젝트의 루트디렉토리에서 환경변수 설정을 해줘야 credential에러가 안생김
+        // The query to send to the dialogflow agent
+        text: req.body.text,
+        // The language used by the client (en-US)
+        languageCode: languageCode,
+      },
+    },
+  };
+
+  // Send request and log result
+  const responses = await sessionClient.detectIntent(request);
+  console.log("Detected intent");
+  const result = responses[0].queryResult;
+  console.log(`  Query: ${result.queryText}`);
+  console.log(`  Response: ${result.fulfillmentText}`);
+
+  res.send(result);
+});
+
+// 이벤트 쿼리 라우트
+module.exports = router;
